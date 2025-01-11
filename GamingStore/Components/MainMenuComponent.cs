d95416111bc1054f93cc393a87ec720da73928fd
Stanislav Lazov <stanislav_lazov1@abv.bg>
2025-01-11 23:23:01 +0200

    Adding the Main Menu component

---
+++ GamingStore/Components/MainMenuComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamingStore.Components
+{
+    public class MainMenuComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
